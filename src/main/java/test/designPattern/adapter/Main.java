7bfce2a561615f1ca71baa69ed2f4950890dc077
Kim Taekyung <seaking7@gmail.com>
2022-05-22 21:20:41 +0900

    adapter 패턴

---
+++ src/main/java/test/designPattern/adapter/Main.java
@@ -0,0 +1,9 @@
+package test.designPattern.adapter;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Print print = new PrintBanner("hello");
+        print.printString();
+    }
+}
---
+++ src/main/java/test/designPattern/adapter/Print.java
@@ -0,0 +1,7 @@
+package test.designPattern.adapter;
+
+public interface Print {
+
+    public void printWeek();
+    public void printString();
+}
